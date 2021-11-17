b40df8783352d68afa0b88171b597e07e8e255e0
Lime <limkosha.02@gmail.com>
2021-11-17 12:57:43 +0600

    Initial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Амантай Алима on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

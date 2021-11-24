6d1d07bee3e846e0f1f649c18a98a85f25da3ad0
Lime <limkosha.02@gmail.com>
2021-11-24 15:27:55 +0600

    added print

---
+++ Photos/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Photos
+//
+//  Created by Амантай Алима on 24.11.2021.
+//
+
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+}
+
--- Photos/ViewController.swift
+++ Photos/ViewController.swift
@@ -1,19 +1,53 @@
 //
-//  ViewController.swift
-//  Photos
+//  CollectionsViewController.swift
+//  Lab1
 //
 //  Created by Амантай Алима on 17.11.2021.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
+    
+    var images = [UIImage]()
+    let countCells = 3
+    let offset: CGFloat = 3.0
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        for i in 0...20 {
+            let image = UIImage(named: "image\(i)")!
+            images.append(image)
+        }
+        print("Hello")
     }
-
-
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! CollectionViewCell
+        let image = images[indexPath.item]
+        cell.photoView.image = image
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let frameCV = collectionView.frame
+        
+        let widthCell = frameCV.width / CGFloat(countCells)
+        let heightCell = widthCell
+        let spacing = CGFloat(countCells + 1) * offset / CGFloat(countCells)
+        return CGSize(width: widthCell - spacing , height: heightCell - (offset * 3) )
+    }
 }
 
